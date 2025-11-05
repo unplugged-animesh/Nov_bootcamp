344d0c737e2e03c42382a6a5ef0728064cc3f97f
unplugged-aniimesh <sharmaanimesh14@gmail.com>
2025-11-05 20:20:57 +0530

    push

--- app.py
+++ app.py
@@ -1,21 +1,85 @@
-from flask import Flask
+from sqlalchemy import or_
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from Models.model import *
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
 
 
-app=Flask(__name__)
-app.config['SECRET_KEY']="East"
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///grocery_store.sqlite3"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'East'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grocery_store.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+
 
 with app.app_context():
     db.create_all()
     
 #admin-credentials:for admin creation
+
+@app.route('/',methods=['GET'])
+def home():
+    return redirect(url_for('logout'))
+
+
+@app.route('/signup',methods=['GET',"POST"])
+def signup():
+    if request.method=="POST":
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        admin=False
+        
+        if request.form.get('admin_key')=="Asharma":
+            admin=True
+            
+        try:
+            user=User(username=username,password=password,email=email,admin=admin)
+            db.session.add(user)
+            db.session.commit()
+            flash("your account has been created Successfully")
+            return redirect (url_for('login'))
     
+        except IntegrityError:
+            db.session.rollback()
+            flash('Username or email already Exists!')
+            return redirect(url_for('signup'))
+        
+    return render_template('signup.html')
+
+@app.route('/login',methods=['GET',"POST"])
+def login():
+    if request.method=="POST":
+        username_or_email=request.form['username']
+        password=request.form['password']
+        
+        user=User.query.filter(
+            or_(User.username==username_or_email, User.email==username_or_email)).first()
+        
+        
+        if user and user.password==password:
+            session['user_id']=user.id
+            return redirect(f'/dashboard/{user.id}')
+            
+        else:
+            error_msg="Invalid Username or password"
+            if not user:
+                error_msg="No user found with the provided username or email"
+            return render_template('login.html',error_msg=error_msg)
+        
+    return render_template('login.html')
+        
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
     
+
+
+
 if __name__=="__main__":
     app.run(port=5000,debug=True)
     
